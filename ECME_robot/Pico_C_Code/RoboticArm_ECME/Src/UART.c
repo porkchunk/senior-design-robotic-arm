f547dc40c5acc7f05d3f034f4e65fa3e1bbadaae
Ayden Schmidt <aydenschmidt11@gmail.com>
2024-10-31 13:00:12 -0500

    Beginning UART code

--- ECME_robot/Pico_C_Code/RoboticArm_ECME/Src/UART.c
+++ ECME_robot/Pico_C_Code/RoboticArm_ECME/Src/UART.c
@@ -1,2 +1,12 @@
 #include "UART.h"
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
 
+void UART_initialization(){
+    gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
+    gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
+
+    uart_init(uart0, 115200);
+
+    
+}
